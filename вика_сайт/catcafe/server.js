95bc320020b098eb1e553c6fe645e80a7781871e
murrrly <aleksandragajnullina66@gmail.com>
2026-02-16 22:06:14 +0300

    че

--- вика_сайт/catcafe/server.js
+++ вика_сайт/catcafe/server.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: 'cat12345',
     database: 'catcafe_db'
 });
 
