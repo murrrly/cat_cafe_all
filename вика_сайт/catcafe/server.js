780d40f221965cb116d5eb1207556d41fc399603
vika <tugashova16@mail.ru>
2026-03-17 14:32:19 +0300

    17 число 2.0

--- вика_сайт/catcafe/server.js
+++ вика_сайт/catcafe/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Подключение к БД дддд
+// Подключение к БД дддддддд
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
